c3fc17f777b52e69554a3e6214c90e74a67be458
Cgl <cagilulusahin@gmail.com>
2015-12-17 17:43:34 +0200

    fixed and commented

--- seshdash/tests.py
+++ seshdash/tests.py
@@ -1,24 +1,32 @@
 from django.test import TestCase
 from seshdash.models import Sesh_Alert, Alert_Rule, Sesh_Site, BoM_Data_Point as Data_Point
 from seshdash.utils import alert
-from datetime import datetime
+from django.utils import timezone
 
+# This test case written to test alerting module.
+# It aims to test if the system sends an email and creates an Sesh_Alert object when an alert is triggered.
 class AlertTestCase(TestCase):
-    site = Sesh_Site.objects.create(site_name=u"Test site", comission_date=datetime(2015, 12, 11, 22, 0),
+    def setUp(self):
+        self.site = Sesh_Site.objects.create(site_name=u"Test site", comission_date=timezone.datetime(2015, 12, 11, 22, 0),
                                     location_city=u"kigali", location_country=u"rwanda", latitude=2.0, longitude=-2.0,
                                     installed_kw=123.0, number_of_pv_strings=12, Number_of_panels=12, vrm_user_id=u"asdasd",
                                     vrm_password=u"asdads", vrm_site_id=213, battery_bank_capacity=12321, has_genset=True,
                                     has_grid=True)
-    data_point = Data_Point.objects.create(site=site, soc=35.5, battery_voltage=20,
-                                               time=datetime.now(),AC_input=0.0,
-                                               AC_output=15.0,AC_Load_in=0.0,
-                                               AC_Load_out=-0.7)
-    def setUp(self):
+        self.data_point = Data_Point.objects.create(site=self.site, soc=35.5, battery_voltage=20,
+                                    time=timezone.now(),AC_input=0.0,
+                                    AC_output=15.0,AC_Load_in=0.0,
+                                    AC_Load_out=-0.7)
         Alert_Rule.objects.create(site = self.site, check_field="soc", value=30, operator="gt")
         Alert_Rule.objects.create(site = self.site, check_field="soc", value=35.5, operator="eq")
-        Alert_Rule.objects.create(site = self.site, check_field="battery_voltage", value=25, operator="lt")
+        Alert_Rule.objects.create(site = self.site, check_field="battery_voltage", value=25, operator="lt",send_mail=False)
 
     def test_alert_fires(self):
         """ Alert working correctly"""
-        self.assertEqual(alert.alert_check(self.data_point),"sent mail")
-        #self.assertEqual(cat.speak(), 'The cat says "meow"')
+        alert.alert_check(self.data_point)
+        alerts_created = Sesh_Alert.objects.filter(site=self.site)
+        # test if necessary alerts has triggered and if alert objects saved
+        self.assertEqual(alerts_created.count(),3)
+        # test if alert mails has been sent
+        self.assertEqual(alerts_created.filter(alertSent=True).count(),2)
+
+        # TODO add negative test cases
--- seshdash/utils/send_mail.py
+++ seshdash/utils/send_mail.py
@@ -3,7 +3,10 @@ from django.template.loader import get_template
 from django.template import Context
 import traceback
 
-def send_mail(subject,to_email,content):
+# example: send_mail("Hello World", [me@gmail.com,you@gmail.com],"Here is your email")
+# Sends and email to me@gmail.com and you@gmail.com addresses an email
+# with subject "Hello World" and body including "Here is your email"
+def send_mail(subject,list_of_recipients,content):
     try:
         plaintext = get_template('mail/site_mail.txt')
         htmly     = get_template('mail/site_mail.html')
@@ -13,8 +16,10 @@ def send_mail(subject,to_email,content):
         from_email = "seshdash@gmail.com"
         text_content = plaintext.render(d)
         html_content = htmly.render(d)
-        msg = EmailMultiAlternatives(subject, text_content, from_email, [to_email])
+        msg = EmailMultiAlternatives(subject, text_content, from_email, list_of_recipients)
         msg.attach_alternative(html_content, "text/html")
         msg.send()
+        return True
     except:
         traceback.print_exc()
+        return False
