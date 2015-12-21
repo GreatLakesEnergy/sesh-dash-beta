35f71675b070b615a8524c38849535142adcb31e
Alp Tilev <atilev@gmail.com>
2015-12-21 12:08:03 +0200

    Fixed user email permissioning emails will now be sent to users of system based on permissions

--- seshdash/utils/alert.py
+++ seshdash/utils/alert.py
@@ -1,6 +1,8 @@
 from seshdash.models import Sesh_Site,Site_Weather_Data,BoM_Data_Point, Alert_Rule, Sesh_Alert
 from seshdash.utils.send_mail import send_mail
 from django.utils import timezone
+from guardian.shortcuts import get_users_with_perms
+import logging
 
 # Sends an email if the received data point fails to pass the defined rules for its site.
 
@@ -17,13 +19,19 @@ def alert_check(data_point):
            'gt': lambda x,y: x>y,
            'eq' : lambda x,y: x==y,
     }
+    recipients = []
     rules = Alert_Rule.objects.filter(site = data_point.site)
     for rule in rules:
         real_value = getattr(data_point,rule.check_field)
         if ops[rule.operator](real_value,rule.value):
             content = "site:%s\nrule:%s '%s' %s --> found %s " %(data_point.site.site_name,rule.check_field,rule.operator,rule.value,real_value)
             # TODO rule object should have the list of related persons to send alert mail
-            recipients = ["seshdash@gmail.com",]
+            users = get_users_with_perms(data_point.site)
+            for user in users:
+                recipients.append(user.email)
+            logging.debug("emailing %s" %recipients)
+            print "emailing %s"%recipients
+            # recipients = ["seshdash@gmail.com",]
             alert_obj = Sesh_Alert.objects.create(site = data_point.site, alert=rule, date=timezone.now(),
                                                   isSilence=True,alertSent=rule.send_mail)
             if rule.send_mail:
