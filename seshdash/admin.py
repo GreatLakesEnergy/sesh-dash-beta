4f2d05de5166c6351aba1ecb586c28edc5a3795b
Alp Tilev <atilev@gmail.com>
2015-12-24 12:36:13 +0200

    added model filtering

--- seshdash/admin.py
+++ seshdash/admin.py
@@ -6,15 +6,21 @@ from .models import Sesh_Site, BoM_Data_Point, Sesh_Alert , Alert_Rule, Sesh_Use
 
 class Sesh_SiteAdmin(GuardedModelAdmin):
     list_display = ('site_name','location_city','installed_kw','battery_bank_capacity','comission_date')
+    list_filter = ('installed_kw','location_city', 'installed_kw')
 
 class BoM_Data_PointAdmin(GuardedModelAdmin):
     list_display= ('site','time','soc','battery_voltage','AC_input', 'AC_output', 'AC_Load_in', 'AC_Load_out', 'pv_production', 'inverter_state', 'genset_state', 'relay_state','trans',)
 
 class Alert_RuleAdmin(GuardedModelAdmin):
     list_display = ('site','check_field','operator','value')
+    list_filter = ('site','operator')
+
+class Sesh_AlertAdmin(GuardedModelAdmin):
+    list_display = ('site','alert','date','isSilence')
+    list_filter = ('site','isSilence', 'date')
 
 admin.site.register(Sesh_Site,Sesh_SiteAdmin)
 admin.site.register(BoM_Data_Point,BoM_Data_PointAdmin)
 admin.site.register(Alert_Rule,Alert_RuleAdmin)
 #admin.site.register(Sesh_User)
-admin.site.register(Sesh_Alert)
+admin.site.register(Sesh_Alert,Sesh_AlertAdmin)
