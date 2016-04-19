9e920db649580ea0ad627708b2e87ff7f23eb16d
Uwonkunda Patrick <patrick@localhost.localdomain>
2016-04-19 10:51:02 +0200

    Created ui for historical data

--- seshdash/sesh-ui/js/historical.js
+++ seshdash/sesh-ui/js/historical.js
@@ -20,23 +20,56 @@ $(function(){
 
     var csrftoken = getCookie('csrftoken');
 
-    $.post('/historical-data', {csrfmiddlewaretoken:csrftoken}, function(data) {
+
+    var elementChart = '',
+        wholeChart = '',
+        historicalLoader = $('.historical-loader'),
+        chartContainer = $('.chart-data-container');
+
+    $.post('/historical_data', {csrfmiddlewaretoken:csrftoken}, function(data) {
+        alert(data);
         
+        chartData = JSON.parse(data);
+        
+        for(i = 0; i < chartData.length; i++) { // For each site
+            var siteName = chartData[i].site_name,
+                siteData = chartData[i].site_historical_data,
+                chartId = 'chart' + chartData[i].site_id;
+
+            elementChart = '<div class="panel panel-default">'
+            elementChart += '<div class="panel-heading"><strong>' + siteName + '</strong></div>';
+            elementChart += '<div class="panel-body"><div class="chart-container" id="' + chartId + '"></div></div> ';
+            elementChart += '<div class="panel-footer">'
+            elementChart += '<div class="row hist-extra-info">'
+            elementChart += '<div class="col-sm-4 extra-info-entity"><h4>Extra Info one</h4></div>';
+            elementChart += '<div class="col-sm-4 extra-info-entity"><h4>Extra Info Two</h4></div>';
+            elementChart += '<div class="col-sm-4 extra-info-entity"><h4>Extra Info Three</h4></div>';
+            elementChart += '</div></div></div>';
+           
+            wholeChart += elementChart;
+            
+        }
+          
+        // Append the string containing all the site holders
+        chartContainer.append(wholeChart);
+        
+        for(i = 0; i < chartData.length; i++) { // For each site generate chart
+
+            var siteData = chartData[i].site_historical_data,
+                chartId = 'chart' + chartData[i].site_id;
+            
+            var chart = calendarHeatmap()
+                        .data(siteData)
+                        .selector('#' + chartId);
+               
+            chart(); 
+            
+            historicalLoader.hide();
+        }
+
     });
     
-    var chartData = [
-        {date: new Date(Date.parse('2016-01-01')), count: 10},
-        {date: new Date(Date.parse('2016-01-02')), count: 20},
-        
-    ];  
-
-    chart = calendarHeatmap()
-            .data(chartData)
-            .selector('.chart-container');
-  
-    chart();
-
-    var chartContainer = $('.chart-data-container');
+    
 
 
 });
