7dba9410038fdc2c7558cb19b0907f77e87d1c6d
rawjazz <rogerkabeho@gmail.com>
2016-05-09 15:03:11 +0200

    !

--- seshdash/static/sesh-ui/js/time_series_graph.js
+++ seshdash/static/sesh-ui/js/time_series_graph.js
@@ -1,14 +1,10 @@
 
 var csrftoken = getCookie('csrftoken');
 
-var measurement_default_value;
-var time_default_value;
-$(document).ready(function(){
-
+function graph() {
     measurement_value = $('#measurements_dropdown').val();
-    time_default_value = $('#time_dropdown').val();
-
-    $.post('/time_series',{csrfmiddlewaretoken:csrftoken,'measurement':measurement_value,'time':time_default_value,'active_id':active_site_id},function(data){
+    time_value = $('#time_dropdown').val();
+  $.post('/time_series',{csrfmiddlewaretoken:csrftoken,'measurement':measurement_value,'time':time_value,'active_id':active_site_id},function(data){
                  var data_values = JSON.parse(data)
                  si_units = data_values.units;
                  graph_data_values=data_values.graph_values;
@@ -17,7 +13,6 @@ $(document).ready(function(){
                   for(var i =0 , l= graph_data_values.length ; i<l ;i++){
                       graph_data_values[i][0] = graph_data_values[i][0] * 1000;
                  }
-                  
             
          $('#time_series_graph').highcharts({
            chart : {
@@ -62,65 +57,14 @@ $(document).ready(function(){
                
           });
      });
-});
+}
 
-var measurement_value;
-var time_value;
-$('#update-butt').click(function(){ 
-         measurement_value = $('#measurements_dropdown').val();
-         time_value = $('#time_dropdown').val();
- $.post('/time_series',{csrfmiddlewaretoken:csrftoken,'measurement':measurement_value,'time':time_value,'active_id':active_site_id},function(data){
+  $('#update-butt').click(function(){
+    graph()
+   });
+
+  $(document).ready(function(){
+    graph()
+  });
+
                 
-                 data_values = JSON.parse(data)
-                 si_units = data_values.units  
-                 graph_data_values = data_values.graph_values
-
-                 for(var i =0 , l= graph_data_values.length ; i<l ;i++){
-                     graph_data_values[i][0] = graph_data_values[i][0] * 1000;
-                 }
-                  
-            
-
-          $('#time_series_graph').highcharts({
-              chart : {
-                zoomtype : 'x'
-           },
-           title : {
-                text: 'Time Series Graph '
-           },
-           xAxis:{
-                 type : 'datetime'
-               },
-           yAxis : [{
-                   labels : {
-                         format : '{value}' + si_units,
-                         style : {
-                               color : Highcharts.getOptions().colors[1]
-                         },
-                     },
-                   title : {
-                         text : measurement_value,
-                   },
-           }],
-           legend : {
-                layout : 'vertical',
-                align : 'left',
-                x:40,
-                varticalAlign: 'top',
-                y :-350,
-                floating:true,
-           },
-           series :[{
-                 name : measurement_value,
-                 type : 'spline',
-                 data : graph_data_values,
-                 tooltip : {
-                        valueSuffix : si_units
-                 },
-            }],
-
-         });
-     });
-});
-       
-                  
