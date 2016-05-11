7414ede26e15c0fd18aacd6e360dab9863ac1127
Alp Tilev <atilev@gmail.com>
2016-05-11 10:47:28 +0200

    adding extra check into security

--- seshdash/tasks.py
+++ seshdash/tasks.py
@@ -546,6 +546,9 @@ def rmc_status_update():
     sites = Sesh_Site.objects.all()
     for site in sites:
         latest_dp = BoM_Data_Point.objects.filter(site=site).order_by('time').first()
+        if not latest_dp:
+            logging.warning("RMC STATUS: No DP found for site")
+            return 0
         last_contact = time_utils.get_timesince_seconds(latest_dp.time)
         tn = timezone.now()
         last_contact_min = last_contact / 60
