8c85c94a881a46937d8c7e05eada719fa4e153eb
FRANK <you@example.com>
2016-05-31 16:11:43 +0200

    fixings merged graphs

--- seshdash/views.py
+++ seshdash/views.py
@@ -770,7 +770,7 @@ def graphs(request):
         active_id = request.POST.get('active_site_id','')
         active_site = Sesh_Site.objects.filter(id=active_id)
         time_delta_dict = {'24h':{'hours':24},'7d':{'days':7},'30d':{'days':30}}
-        time_bucket_dict = {'24h':'30m','7d':'12h','30d':'1d'}
+        time_bucket_dict = {'24h':'1h','7d':'1d','30d':'5d'}
 
         # Checking for a valid site_id
         if active_site != []:
@@ -784,8 +784,6 @@ def graphs(request):
             time_bucket=time_bucket_dict[time]
             SI_units = BoM_Data_Point.SI_UNITS
             SI_unit = SI_units.get(choice,'V')		
-           # SI_unit = get_measurement_unit(choice)
-            print SI_unit
             # creating an influx instance
             client = Influx()
             
