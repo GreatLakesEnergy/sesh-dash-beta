cc2b4798c88b96d6dd56fa2576688a29f6306051
Alp Tilev <atilev@gmail.com>
2016-06-03 12:03:00 +0200

    Revert "squashing migrations"

---
+++ seshdash/migrations/0055_merge.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('seshdash', '0053_auto_20160419_1334'),
+        ('seshdash', '0046_auto_20160421_0916'),
+        ('seshdash', '0054_auto_20160418_1656'),
+    ]
+
+    operations = [
+    ]
