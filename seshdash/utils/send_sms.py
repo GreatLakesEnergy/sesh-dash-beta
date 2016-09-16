658ded3a93d8f63243a2b76b52f0f67175460a82
upatricck <patrickuwonkunda@gmail.com>
2016-09-16 13:13:47 +0200

    fixed bug in sending sms

--- seshdash/utils/send_sms.py
+++ seshdash/utils/send_sms.py
@@ -2,8 +2,17 @@ from clickatell.rest import Rest
 from django.conf import settings
 from django.template.loader import get_template
 from django.conf import settings
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 def send_sms(recipients, context, sms_template='alert'):
+    response = False
+
+    if not recipients:
+        logger.debug("No recipients for alert")
+        return False
 
     if settings.CLICKATELL_KEY == 'your_clickatell_key' or settings.DEBUG:
         clicksms = False
@@ -14,10 +23,10 @@ def send_sms(recipients, context, sms_template='alert'):
     sms_template = get_template('seshdash/sms/%s_sms.txt'%sms_template)
     sms = sms_template.render(context)
     
-    response = False
     if clicksms:
         if not settings.DEBUG:
             response = clicksms.sendMessage(recipients, sms)
+            print "The clickatell response is: %s" % response
             response = not response[0]['errorCode']
     
     return response
