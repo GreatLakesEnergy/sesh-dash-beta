2fa58d69b98e87c61772a150196d74de11875ef1
upatricck <patrickuwonkunda@gmail.com>
2016-11-15 13:29:17 +0200

    making slack token optional

--- seshdash/models.py
+++ seshdash/models.py
@@ -36,7 +36,7 @@ class VRM_Account(models.Model):
 class Sesh_Organisation(models.Model):
     name = models.CharField(max_length=40)
     send_slack = models.BooleanField(default=False)
-    slack_token = models.CharField(max_length=100)
+    slack_token = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.name
